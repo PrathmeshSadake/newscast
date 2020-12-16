fd19c955cbf9c7d7e9513c071bc0fa2d987f4860
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-12-16 20:40:28 +0530

    Used WebView for article displaying

---
+++ lib/screens/article_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleView extends StatefulWidget {
+  final String blogUrl;
+
+  ArticleView({@required this.blogUrl});
+
+  @override
+  _ArticleViewState createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  // final Completer<WebViewController> _completer = Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Newscast'),
+        elevation: 0,
+      ),
+      body: Container(
+          child: WebView(
+            initialUrl: widget.blogUrl,
+          )),
+    );
+  }
+}
