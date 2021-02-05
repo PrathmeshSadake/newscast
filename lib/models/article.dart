ca69858a3e52d7c56c4873e0f60a303b01cd3059
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-06 00:29:04 +0530

    created Article class

---
+++ lib/models/article.dart
@@ -0,0 +1,17 @@
+class Article {
+  final String headline;
+  final String description;
+  final String source;
+  final String webUrl;
+  final String imageUrl;
+  final String date;
+
+  Article(
+      {
+      this.headline,
+      this.description,
+      this.source,
+      this.webUrl,
+      this.imageUrl,
+      this.date});
+}
