af54c9bc7c1fb2e26dadb776f1b9bae2dc96ce41
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-06 00:28:30 +0530

    using multiproviders

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
-
-import 'package:newscast/screens/home.dart';
-import 'package:newscast/screens/splash_screen.dart';
+import 'package:newscast/models/category.dart';
+import 'package:newscast/providers/categories.dart';
+import 'package:newscast/providers/news.dart';
+import 'package:provider/provider.dart';
+import './screens/homepage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Newscast',
-      theme: ThemeData(
-        primaryColor: Colors.black,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: News(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Categories(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Color(0xFF93B2FE),
+          primaryColorLight: Color(0xFFCCCFF5),
+          accentColor: Color(0xFFAC7FFC),
+          textTheme: TextTheme(),
+          backgroundColor: Color(0xFFF9FAFE),
+          buttonColor: Color(0xFFF595B8),
+        ),
+        home: Homepage(),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/home': (context) => Home(),
-      },
     );
   }
 }
+
+/*
+COLOR SCHEMES 
+white : #F9FAFE
+blue : #93B2FE
+purple : #AC7FFC
+pink : #F595B8
+light: #CCCFF5
+ */
